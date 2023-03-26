058442d4a19c4e7581ec5b707ec7a2dbc65dbfd0
nakajima-satoru <nakajima@email.com>
2023-03-26 13:54:41 +0900

    3/26 Adding test samples

--- test/test.php
+++ test/test.php
@@ -2,9 +2,30 @@
 
 require "../src/Validate.php";
 
-use Reald\Validator\Validate;
+use Reald\Validate\Validate;
 
 $v = new Validate;
 
+$post = [];
+
+$rules = [
+    "name" => [
+        [
+            "rule" => "required",
+            "message" => "no name entered",
+        ],
+        [
+            "rule" => ["maxLength", 255],
+            "message" => "name exceeds 255 characters",
+        ],
+    ],
+    "code" => [
+        [
+            "rule" => "required",
+            "message" => "no code entered",
+        ],
+    ],
+];
+
 echo "<pre>";
-print_r($v->verify([])->toJuge());
+print_r($v->verify($post, $rules)->toArray());
